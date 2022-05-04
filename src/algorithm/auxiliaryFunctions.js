fbe31401811d49fae3e2901e441d8df71b17e1b9
Tanya Kulikova <tanya.kulikova.02@mail.ru>
2022-05-04 03:45:38 +0300

    fixed decomposition

--- src/algorithm/auxiliaryFunctions.js
+++ src/algorithm/auxiliaryFunctions.js
@@ -1,5 +1,3 @@
-
-
 function checkingComponent(component, matching, graph) {
     let amount = 0;
     for (let node of component) {
@@ -25,95 +23,96 @@ function checkingComponent(component, matching, graph) {
     }
     return false;
 }
+
 module.exports = {
 
-checking:function(graph, matching) {
-    for (let i = 0; i < graph.components.length; ++i) {
-        if (checkingComponent(graph.components[i], matching, graph)) {
-            return true;
+    checking: function (graph, matching) {
+        for (let i = 0; i < graph.components.length; ++i) {
+            if (checkingComponent(graph.components[i], matching, graph)) {
+                return true;
+            }
         }
-    }
-    return false;
-},
+        return false;
+    },
 
-finalOutput:function(matching) {
-    document.getElementById('button-create').disabled = false;
-    document.getElementById('button-generate').disabled = false;
-    document.getElementById('algoSvg').innerText += "Matching is found!\n";
-    document.getElementById('algoSvg').innerText += "Matching Size: " + matching.size + "\n";
-},
+    finalOutput: function (matching) {
+        document.getElementById('button-create').disabled = false;
+        document.getElementById('button-generate').disabled = false;
+        document.getElementById('algoSvg').innerText += "Matching is found!\n";
+        document.getElementById('algoSvg').innerText += "Matching Size: " + matching.size + "\n";
+    },
 
-correctingNodesAndEdges:function(cy) {
-    for (let node of cy.nodes()) {
-        node.unselect();
-        node.unselectify();
-    }
-    for (let edge of cy.edges()) {
-        edge.unselect();
-        edge.unselectify();
-    }
-},
-
-
-outputMatching:function(matching) {
-    for (let edge of matching) {
-        document.getElementById('algoSvg').innerText += edge.toString() + " ";
-    }
-},
-
-findOtherNodeInMatching:function(edges, vertex) {
-    for (let edge of edges) {
-        if (edge.firstVertex === vertex) {
-            return edge.secondVertex;
+    correctingNodesAndEdges: function (cy) {
+        for (let node of cy.nodes()) {
+            node.unselect();
+            node.unselectify();
         }
-        if (edge.secondVertex === vertex) {
-            return edge.firstVertex;
+        for (let edge of cy.edges()) {
+            edge.unselect();
+            edge.unselectify();
         }
-    }
-    return null;
-},
+    },
 
-containsEdgeWithNode:function(path, vertex) {
-    for (let i = 0; i < path.length; ++i) {
-        if (path[i] === vertex)
-            return true;
-    }
-    return false;
-},
-    returnAugPath:function(graph, rootMap, parentMap, heightMap, v, w) {
-    let augPath = [];
-    let curr = v;
-    while (curr != null) {
-        augPath.push(curr);
-        curr = parentMap.get(curr);
-    }
-    augPath.reverse();
-    curr = w;
-    while (curr != null) {
-        augPath.push(curr);
-        curr = parentMap.get(curr);
-    }
-    return augPath;
-},
-findOutgoingIndex:function(vertex, blossom, graph) {
-    for (let i = 0; i < blossom.length; i++) {
-        if (graph.getEdge(vertex, blossom[i]) != null) {
-            return i;
-        }
-    }
-    document.getElementById('algoSvg').innerText += "Blossom lifting error!\n\n";
-    return -1;
-},
 
-    addAltEdges:function (augPath, graph, matching) {
-    for (let i = 0; i < augPath.length - 1; i += 1) {
-        if (i % 2 === 0) {
-            matching.add(graph.getEdge(augPath[i], augPath[i + 1]));
+    outputMatching: function (matching) {
+        for (let edge of matching) {
+            document.getElementById('algoSvg').innerText += edge.toString() + " ";
         }
-        if (i % 2 === 1) {
-            matching.delete(graph.getEdge(augPath[i], augPath[i + 1]));
+    },
+
+    findOtherNodeInMatching: function (edges, vertex) {
+        for (let edge of edges) {
+            if (edge.firstVertex === vertex) {
+                return edge.secondVertex;
+            }
+            if (edge.secondVertex === vertex) {
+                return edge.firstVertex;
+            }
         }
-    }
-    return matching;
-},
+        return null;
+    },
+
+    containsEdgeWithNode: function (path, vertex) {
+        for (let i = 0; i < path.length; ++i) {
+            if (path[i] === vertex)
+                return true;
+        }
+        return false;
+    },
+    returnAugPath: function (graph, rootMap, parentMap, heightMap, v, w) {
+        let augPath = [];
+        let curr = v;
+        while (curr != null) {
+            augPath.push(curr);
+            curr = parentMap.get(curr);
+        }
+        augPath.reverse();
+        curr = w;
+        while (curr != null) {
+            augPath.push(curr);
+            curr = parentMap.get(curr);
+        }
+        return augPath;
+    },
+    findOutgoingIndex: function (vertex, blossom, graph) {
+        for (let i = 0; i < blossom.length; i++) {
+            if (graph.getEdge(vertex, blossom[i]) != null) {
+                return i;
+            }
+        }
+        document.getElementById('algoSvg').innerText += "Blossom lifting error!\n\n";
+        return -1;
+    },
+
+    addAltEdges: function (augPath, graph, matching) {
+        for (let i = 0; i < augPath.length - 1; i += 1) {
+            if (i % 2 === 0) {
+                matching.add(graph.getEdge(augPath[i], augPath[i + 1]));
+            }
+            if (i % 2 === 1) {
+                matching.delete(graph.getEdge(augPath[i], augPath[i + 1]));
+            }
+        }
+        return matching;
+    },
 }
